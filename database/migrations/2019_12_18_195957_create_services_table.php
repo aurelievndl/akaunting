2278f5d2c78a2ba65be87f86702428543fe638c2
Aurélie Vandaele <aurelie.vndl@gmail.com>
2019-12-18 21:17:22 +0100

    Add services table

---
+++ database/migrations/2019_12_18_195957_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id');
+            $table->string('name');
+            $table->boolean('enabled');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('company_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
