117061a23717f0d86117e57e5e4ee0846f981923
Aurélie Vandaele <aurelie.vndl@gmail.com>
2019-12-18 20:56:43 +0100

    Set default attributes for item

--- app/Jobs/Common/CreateItem.php
+++ app/Jobs/Common/CreateItem.php
@@ -28,6 +28,39 @@ class CreateItem extends Job
     {
         $item = Item::create($this->request->all());
 
+        if( !$item->extra_attributes->get('amount')) {
+            $item->extra_attributes->amount = [
+                'value' => '',
+                'additional_price' => '',
+            ];
+            $item->save();
+        }
+
+        if( !$item->extra_attributes->get('material')) {
+            $item->extra_attributes->material = [
+                'type' => '',
+                'additional_price' => '',
+            ];
+            $item->save();
+        }
+
+        if( !$item->extra_attributes->get('size')) {
+            $item->extra_attributes->size = [
+                'width' => '',
+                'height' => '',
+                'additional_price' => '',
+            ];
+            $item->save();
+        }
+
+        if( !$item->extra_attributes->get('structure')) {
+            $item->extra_attributes->structure = [
+                'value' => '',
+                'additional_price' => '',
+            ];
+            $item->save();
+        }
+
         // Upload picture
         if ($this->request->file('picture')) {
             $media = $this->getMedia($this->request->file('picture'), 'items');
